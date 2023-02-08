ec7a12ac323611d275a342e1fbdc8c5a4ec0c024
unanmed <1319491857@qq.com>
2023-02-08 15:17:58 +0800

    加载

---
+++ src/game/loader/load.ts
@@ -0,0 +1,15 @@
+const images: Record<string, HTMLImageElement> = {};
+const bgms: Record<string, HTMLAudioElement> = {};
+const sounds: Record<string, AudioBuffer> = {};
+
+export function loadImage(name: string, path: string) {}
+
+export function loadBgm(name: string, path: string) {}
+
+export function loadSound(name: string, path: string) {}
+
+export function getImage(name: string) {}
+
+export function getBgm(name: string) {}
+
+export function getSound(name: string) {}
--- src/game/loader/loader.ts
+++ src/game/loader/loader.ts
@@ -5,6 +5,8 @@ export class Loader {
     private tasks: LoadTask[] = [];
     /** 绑定的响应式变量 */
     private bindedRef?: Ref<number>;
+    /** 加载完成后执行的函数 */
+    private onFulfill?: () => void;
 
     /** 加载进度，范围0~100 */
     private _progress: number = 0;
@@ -35,53 +37,77 @@ export class Loader {
      * 添加加载任务
      * @param task 添加的加载任务列表
      */
-    addTask(...task: LoadTask[]): void {}
+    addTask(...task: LoadTask[]): void {
+        this.tasks.push(...task);
+    }
 
     /**
      * 将加载进度绑定到一个响应式变量上
      * @param target 绑定目标
      */
-    bindProgress(target: Ref<number>): void {}
+    bindProgress(target: Ref<number>): void {
+        this.bindedRef = target;
+    }
 
     /**
      * 开始执行加载任务
      */
-    start(): void {}
+    start(): Loader {
+        let loaded = 0;
+        Promise.all(
+            this.tasks.map(
+                v =>
+                    new Promise<void>(res => {
+                        v.start().then(() => {
+                            loaded++;
+                            this.progress = loaded / this.tasks.length;
+                            res();
+                        });
+                    })
+            )
+        ).then(this.onFulfill);
+        return this;
+    }
 
     /**
      * 加载完成后执行函数
      */
-    then(fn: () => void): void {}
+    then(fn: () => void): void {
+        this.onFulfill = fn;
+    }
 }
 
 export class LoadTask {
-    /** 加载进度，范围0~100 */
-    private _progress: number = 0;
+    /** 加载函数 */
+    private fn?: () => Promise<void>;
+    /** 加载完成后执行的函数 */
+    private onFulfill?: () => void;
 
-    /** 加载进度，范围0~100 */
-    public get progress(): number {
-        return this._progress;
+    constructor(fn?: () => Promise<void>) {
+        this.fn = fn;
     }
-
-    private set progress(v: number) {
-        this._progress = Math.ceil(v);
-    }
-
-    constructor(fn?: () => Promise<void>) {}
 
     /**
      * 设置加载时执行的函数
      * @param action 加载时执行的函数
      */
-    setLoadAction(action: Promise<void>): void {}
+    setLoadAction(action: () => Promise<void>): void {
+        this.fn = action;
+    }
 
     /**
      * 开始这个加载任务
      */
-    start(): void {}
+    start(): LoadTask {
+        if (!this.fn) throw new Error(`No loading function set!`);
+        this.fn().then(this.onFulfill);
+        return this;
+    }
 
     /**
      * 加载完成后执行函数
      */
-    then(fn: () => void): void {}
+    then(fn: () => void): void {
+        this.onFulfill = fn;
+    }
 }
